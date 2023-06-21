28ed6776913ae7fca5cedd329f602ecfc2225df6
Evelyn Gaskin <eggaskin@login04.rc.byu.edu>
2023-06-21 16:14:20 -0600

    reformat for modifying backbone layers

--- src/models/ffvt.py
+++ src/models/ffvt.py
@@ -10,19 +10,44 @@ import timm
 # "fit" tells Lightning to run the training loop
 # srun python run.py fit -c config.yaml
 # conda activate hp
-# salloc -N 1 -n 8 --gpus 2 --mem 32G --time 0:30:00 --qos cs
+# salloc -N 2 -n 8 --gpus 2 --mem 32G --time 0:30:00 --qos cs
 
 # linear, crossentropyloss, dropout, softmax, conv2d, layernorm are imported models
 # also resnet (?)
-NUM_LAYERS = 1 # 12
-HIDDEN_SIZE = 8 # 768
+NUM_LAYERS = 2 # 12, 1 doesn't work for testing
+HIDDEN_SIZE = 32 # 768
 DROP_RATE = 0.1 # 0.0
 MLP_DIM = 1 # 3072
+NUM_TOKEN = 12
 
 # Attention layer adapted from https://github.com/huggingface/pytorch-image-models/blob/main/timm/models/vision_transformer.py
 class newAttention(timm.models.vision_transformer.Attention):
+    # transformer gets embedding output
+        # get patch embeddings from Conv2d layer
+        # cls_token, line 182 on
+    # passes to encoder to get encoded and attention weights
+        # encoder passes through LayerNorm and then attention layer 
+        # needs to have 4 dimensions when split into q,k,v
+
+    def __init__(self,dim):
+        super().__init__(dim)
+        self.out = torch.nn.Linear(self.head_dim, self.head_dim)
+        self.proj_dropout = self.proj_drop 
+        self.softmax2 = torch.nn.Softmax(dim=-2)
+
     def forward(self,x):
-        B, N, C = x.shape
+        B, N , C= x.shape # only B, N, no C because diff. dimensions
+        # shape to 200,32,3,8,4, from 19200 - not valid, need more data???
+        # head dim is 4, 8 is num heads, 32 is dim  
+        # only have 200*32*3 tensor
+        # if you want num_heads and head_dim to be 1, headdim= dim//num_heads so 1 = 1/1 or 1 = 32/32
+        # dim
+        # qkv = self.qkv(x) # 200x96
+        # qkv=qkv.reshape(B,N,3,1) # or 32,200,1,3 qkv.permute(2,0,3,1)
+        # qkv = qkv.permute(2,0,3,1)
+        # qkv = qkv.unbind(2)
+        # print(f"len {len(qkv)}") # 200x96 is size (32*3) -> 32x200xnum_headsx3 - no head_dim
+        # print(qkv[0].size())
         qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, self.head_dim).permute(2, 0, 3, 1, 4)
         q, k, v = qkv.unbind(0)
         q, k = self.q_norm(q), self.k_norm(k)
@@ -32,15 +57,16 @@ class newAttention(timm.models.vision_transformer.Attention):
         weights = attn
         attn = self.attn_drop(attn)
         x = attn @ v # context layer
-        # x = x.transpose(1, 2).reshape(B, N, C)
+        #x = x.transpose(1, 2).reshape(B, N, C)
         # x = self.proj(x)
         # x = self.proj_drop(x)
         # return x
     
         # new code for attention
         # context_layer = torch.matmul(attention_probs, value_layer)
+        all_head_size = self.num_heads * self.head_dim/self.num_heads
         context_layer = x.permute(0, 2, 1, 3).contiguous()
-        new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size,)
+        new_context_layer_shape = context_layer.size()[:-2] + (self.head_dim,)
         context_layer = context_layer.view(*new_context_layer_shape)
         attention_output = self.out(context_layer)
         attention_output = self.proj_dropout(attention_output)
@@ -72,13 +98,14 @@ class MLP(torch.nn.Module):
         return x
 
 class Block(torch.nn.Module):
-    def __init__(self):
+    def __init__(self,size):
         super(Block, self).__init__()
         self.attentnorm = torch.nn.LayerNorm(HIDDEN_SIZE,eps=1e-6)
         self.ffnorm = torch.nn.LayerNorm(HIDDEN_SIZE,eps=1e-6)
         self.mLP = MLP()
         self.attn = newAttention(HIDDEN_SIZE) # hidden_size = dim param
         return
+
     def forward(self, x):
         actualx = x
         x = self.attentnorm(x)
@@ -89,42 +116,70 @@ class Block(torch.nn.Module):
         x = savex+x
         return x, weights
 
-# QUESTIONS
-# standardize layer numbers, input sizes?
-
-class Transformer(torch.nn.Module):
-    def __init__(self, size):
-        super(Transformer,self).__init__()
-        # encoder layer
-        self.layer = torch.nn.ModuleList([Block() for i in range(NUM_LAYERS-1)])
+class NewBlock(torch.nn.Module):
+    def __init__(self,layer,size):
+        super(NewBlock, self).__init__()
+        self.layer = layer
         self.maws = MAWS()
-        self.lastlayer = Block()
-        self.encoder = torch.nn.LayerNorm(HIDDEN_SIZE,eps=1e-6) 
-    
-    def forward(self, embeds):
-        # send embedding through encoder, return x, weights
-        tokens=[[] for i in range(embeds.shape[0])]
-        weights = []
-        # now forward function for encoder
-        for layer_block in self.layer:
-            # forward step
-            embed,weight,cont = layer_block(embeds)
-            weights.append(weight)
+        self.attentnorm = torch.nn.LayerNorm(HIDDEN_SIZE,eps=1e-6)
+        self.ffnorm = torch.nn.LayerNorm(HIDDEN_SIZE,eps=1e-6)
+        self.mLP = MLP()
+        self.attn = newAttention(HIDDEN_SIZE) # hidden_size = dim param
+        self.weight = None
+        return
 
-            # feature fusion
-            num,inx = self.maws(weight,cont)
-            for i in range(inx.shape[0]):
-                tokens[i].extend(embed[i,inx[i,:num]])
-        print(tokens.size())
-        # TODO: is tokens empty or no??
-        # more feature fusion
+    def forward(self, embeds):
+        tokens = [[] for i in range(embeds.shape[0])]
+        x = embeds
+        actualx = x
+        x = self.attentnorm(x)
+        x,weights,cont = self.attn(x) # x is hidden_states
+        savex = x+actualx
+        x = self.ffnorm(savex)
+        x = self.mLP(x)
+        x = savex+x
+
+        embed = x
+        self.weight = weights
+        # feature fusion
+        num,inx = self.maws(weight,cont)
+        for i in range(inx.shape[0]):
+            #tokens[i].extend(embed[i,inx[i,:NUM_TOKEN]])
+            self.toextend = [i,embed[i,inx[i,:NUM_TOKEN]]]
+
+        return embed
+
+class LastLayer(torch.nn.Module):
+    def __init__(self,layer,size,tokens):
+        super(LastLayer,self).__init__()
+        self.layer = layer
+        super(Block, self).__init__()
+        self.attentnorm = torch.nn.LayerNorm(HIDDEN_SIZE,eps=1e-6)
+        self.ffnorm = torch.nn.LayerNorm(HIDDEN_SIZE,eps=1e-6)
+        self.mLP = MLP()
+        self.attn = newAttention(HIDDEN_SIZE) # hidden_size = dim param
+        
+        self.encoder = torch.nn.LayerNorm(HIDDEN_SIZE,eps=1e-6) 
+
+        return
+
+    def forward(self, embeds):
         tokens=[torch.stack(token) for token in tokens]
         tokens = torch.stack(tokens).squeeze(1)
         concat = torch.cat((embeds[:,0].unsqueeze(1), tokens), dim=1)
+
+        x = embeds
+        actualx = x
+        x = self.attentnorm(x)
+        x,weights,cont = self.attn(x)
+        savex = x+actualx
+        x = self.ffnorm(savex)
+        x = self.mLP(x)
+        x = savex+x
+        ff_states, ff_weights = x, weights
         # do last layer with selected 'important' tokens from MAWS
-        ff_states, ff_weights, ff_contri = self.lastlayer(concat)
         encoded = self.encoder(ff_states)
-        return encoded, weights # which would be list of weights from each layer
+        return encoded, weights
 
 # mutual attention weight selection
 class MAWS(torch.nn.Module):
@@ -154,16 +209,57 @@ class FFVT(ImageClassifier):
         ImageClassifier.__init__(self, base_conf=base_conf, model_conf=model_conf)
     
     def setup_model(self):
+        # for each backbone block, change to incorporate module
+        hidden_size = self.backbone.embed_dim
+        for i in range(len(self.backbone.blocks)):
+            block = self.backbone.blocks[i]
+            # each block takes embeddings!!
+            self.backbone.blocks[i] = NewBlock(block, hidden_size)
 
-        # need transformer and head
-        self.transformer = Transformer(self.feature_size)
-        self.linear = torch.nn.Linear(self.feature_size, self.num_classes)
-        # self.model_conf.model_kw.update(img_size=448,)
+        # edit last block
+        #block = self.backbone.blocks[-1]
+        #self.backbone.blocks[-1] = LastLayer(block,hidden_size)
+
+        self.lastlayer = Block(hidden_size)
+
+        self.encoder = torch.nn.LayerNorm(hidden_size,eps=1e-6) 
+        
+        # remove the ViT classification head
+        self.backbone.head = torch.nn.Identity()
+
+        # self.head = torch.nn.Sequential( # TODO:
+        #     # torch.nn.BatchNorm1d(hidden_size * 3),
+        #     torch.nn.LayerNorm(hidden_size * 3),
+        #     torch.nn.Linear(hidden_size * 3, 1024),
+        #     # torch.nn.BatchNorm1d(1024),
+        #     torch.nn.LayerNorm(1024),
+        #     torch.nn.ELU(inplace=True),
+        #     torch.nn.Linear(1024, self.model_conf.num_classes),
+        # )
+        self.head = torch.nn.Linear(self.feature_size, self.num_classes)
 
     def forward(self, x):
-        embeds = self.backbone(x)
-        x, weights = self.transformer(embeds)
-        logits = self.linear(x[:,0])
+        # x = x.transpose(-1,-2) # TODO: needed for sizing??
+        breakpoint()
+        x = self.backbone.forward_features(x)
+        # get weights from each block
+        weights = []
+        tokens=[[] for i in range(x.shape[0])]
+        for block in self.backbone.blocks: # TODO: does this work
+            tokens[block.toextend[0]].extend(block.toextend[1])
+            weights.append[block.weight]
+            block.weight = None
+
+        # do last layer here??
+        embeds = x
+        tokens=[torch.stack(token) for token in tokens]
+        tokens = torch.stack(tokens).squeeze(1)
+        concat = torch.cat((embeds[:,0].unsqueeze(1), tokens), dim=1)
+        # do last layer with selected 'important' tokens from MAWS
+        ff_states, ff_weights = self.lastlayer(concat)
+        encoded = self.encoder(ff_states) # normalization
+
+        logits = self.head(x[:,0])
         # get x, weights from transformer
         # get logits/predictions from head (linear layer)
         # classification 
